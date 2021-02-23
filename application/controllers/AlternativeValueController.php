eb4f3dabde5cfc3c0ed9cbda6444e6b476656e4e
dodinovembri <dodinovembri32gmail.com>
2021-02-23 16:03:46 +0700

    update

--- application/controllers/AlternativeValueController.php
+++ application/controllers/AlternativeValueController.php
@@ -6,7 +6,7 @@ class AlternativeValueController extends CI_Controller {
     function __construct()
     {
         parent::__construct();
-        $this->load->model(['UserModel', 'AlternativeModel', 'AlternativeModel', 'CriteriaModel', 'CriterionValueModel']);
+        $this->load->model(['UserModel', 'AlternativeValueModel', 'CriteriaModel', 'CriterionValueModel']);
 
         // check login status and role id  as administrator(0) or not
         if ($this->session->userdata('logged_in') != 1) {
@@ -49,7 +49,7 @@ class AlternativeValueController extends CI_Controller {
         $criteria_criterion = $this->input->post('criteria_criterion');
         $count = count($criteria_criterion);
 
-        $delete = $this->AlternativeModel->destroy_by_alternative($alternative_id);
+        $delete = $this->AlternativeValueModel->destroy_by_alternative($alternative_id);
 
         foreach ($criteria_criterion as $key => $value) {
             $data = explode("&" , $value);
@@ -64,7 +64,7 @@ class AlternativeValueController extends CI_Controller {
                 'criterion_value_id' => $criterion_value_id
             );
 
-            $insert = $this->AlternativeModel->insert($data);
+            $insert = $this->AlternativeValueModel->insert($data);
         }
         $this->session->set_flashdata('success', "Success create criteria!");
         return redirect(base_url("alternative_values/$alternative_id"));
@@ -78,9 +78,9 @@ class AlternativeValueController extends CI_Controller {
 
     public function edit($id)
     {
-        $alternative_value = $this->AlternativeModel->get_data($id)->row();
+        $alternative_value = $this->AlternativeValueModel->get_data($id)->row();
         $data['criterion_value'] = $this->CriterionValueModel->get_criterion_value($alternative_value->criteria_id)->result();
-        $data['alternative_value'] = $this->AlternativeModel->get_data_join($id)->row();
+        $data['alternative_value'] = $this->AlternativeValueModel->get_data_join($id)->row();
 
         $this->load->view('templates/backend/header');
         $this->load->view('alternative_value/edit', $data);
