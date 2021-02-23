93d6dc0144313f9d527df33f9755ce692ef9c8bf
dodinovembri <dodinovembri32@gmail.com>
2021-02-23 07:19:22 +0700

    update

--- spk_ulfa.sql
+++ spk_ulfa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 22, 2021 at 01:01 AM
+-- Generation Time: Feb 23, 2021 at 01:17 AM
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.4.7
 
@@ -70,6 +70,21 @@ CREATE TABLE `alternative_value` (
   `updated_by` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `alternative_value`
+--
+
+INSERT INTO `alternative_value` (`id`, `alternative_id`, `criteria_id`, `criterion_value_id`, `status`, `created_at`, `updated_at`, `created_by`, `updated_by`) VALUES
+(19, 1, 11, 4, 1, NULL, NULL, NULL, NULL),
+(20, 1, 12, 7, 1, NULL, NULL, NULL, NULL),
+(21, 1, 13, 10, 1, NULL, NULL, NULL, NULL),
+(22, 1, 14, 16, 1, NULL, NULL, NULL, NULL),
+(23, 1, 15, 21, 1, NULL, NULL, NULL, NULL),
+(24, 1, 16, 27, 1, NULL, NULL, NULL, NULL),
+(25, 1, 17, 31, 1, NULL, NULL, NULL, NULL),
+(26, 1, 18, 36, 1, NULL, NULL, NULL, NULL),
+(27, 1, 19, 42, 1, NULL, NULL, NULL, NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -120,6 +135,55 @@ CREATE TABLE `criterion_value` (
   `created_by` int(11) DEFAULT NULL,
   `updated_by` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `criterion_value`
+--
+
+INSERT INTO `criterion_value` (`id`, `criteria_id`, `description`, `value`, `status`, `created_at`, `updated_at`, `created_by`, `updated_by`) VALUES
+(1, 11, ' Akreditasi A', 5, 1, NULL, NULL, NULL, NULL),
+(2, 11, ' Akreditasi B', 4, 1, NULL, NULL, NULL, NULL),
+(3, 11, ' Akreditasi C', 3, 1, NULL, NULL, NULL, NULL),
+(4, 11, ' Belum Terakdreditasi', 1, 1, NULL, NULL, NULL, NULL),
+(5, 12, '> 20 Fasilitas', 5, 1, NULL, NULL, NULL, NULL),
+(6, 12, ' 16 - 20 Fasilitas', 4, 1, NULL, NULL, NULL, NULL),
+(7, 12, ' 11 - 15 Fasilitas', 3, 1, NULL, NULL, NULL, NULL),
+(8, 12, ' 6 - 10 Fasilitas', 2, 1, NULL, NULL, NULL, NULL),
+(9, 12, '1 - 5 Fasilitas ', 1, 1, NULL, NULL, NULL, NULL),
+(10, 13, '> 20 Ekstrakurikuler', 5, 1, NULL, NULL, NULL, NULL),
+(11, 13, '16-20 Ekstrakurikuler', 4, 1, NULL, NULL, NULL, NULL),
+(12, 13, '11-15 Ekstrakurikuler ', 3, 1, NULL, NULL, NULL, NULL),
+(13, 13, '6-10 Ekstrakurikuler', 2, 1, NULL, NULL, NULL, NULL),
+(14, 13, '1-5 Ekstrakurikuler', 1, 1, NULL, NULL, NULL, NULL),
+(15, 14, '> 60 Tahun', 5, 1, NULL, NULL, NULL, NULL),
+(16, 14, '46-60 Tahun', 4, 1, NULL, NULL, NULL, NULL),
+(17, 14, '31-45 Tahun', 3, 1, NULL, NULL, NULL, NULL),
+(18, 14, '16-30 Tahun', 2, 1, NULL, NULL, NULL, NULL),
+(19, 14, '1-15 Tahun', 1, 1, NULL, NULL, NULL, NULL),
+(20, 15, '< Rp 250.000', 5, 1, NULL, NULL, NULL, NULL),
+(21, 15, 'Rp 251.000 – Rp 500.000', 4, 1, NULL, NULL, NULL, NULL),
+(22, 15, 'Rp 501.000 – Rp 750.000', 3, 1, NULL, NULL, NULL, NULL),
+(23, 15, 'Rp 751.000 – Rp 1.000.000', 2, 1, NULL, NULL, NULL, NULL),
+(24, 15, '> Rp 1.000.000', 1, 1, NULL, NULL, NULL, NULL),
+(25, 16, '< Rp 2.000.000', 5, 1, NULL, NULL, NULL, NULL),
+(26, 16, 'Rp 2.001.000 – Rp 4.000.000', 4, 1, NULL, NULL, NULL, NULL),
+(27, 16, 'Rp 4.001.000 – Rp 6.000.000', 3, 1, NULL, NULL, NULL, NULL),
+(28, 16, 'Rp 6.001.000 – Rp 8.000.000', 2, 1, NULL, NULL, NULL, NULL),
+(29, 16, '> Rp 8.000.000', 1, 1, NULL, NULL, NULL, NULL),
+(30, 17, 'Di pinggir jalan besar (jalan raya)', 5, 1, NULL, NULL, NULL, NULL),
+(31, 17, 'Di dekat wilayah perkantoran', 4, 1, NULL, NULL, NULL, NULL),
+(32, 17, 'Di dekat wilayah permukiman warga', 3, 1, NULL, NULL, NULL, NULL),
+(33, 17, 'Di pinggir jalan kecil (lorong/gang)', 2, 1, NULL, NULL, NULL, NULL),
+(34, 17, 'Di dekat wilayah pertokoan', 1, 1, NULL, NULL, NULL, NULL),
+(35, 18, 'Terdapat Akses Kendaraan Umum', 5, 1, NULL, NULL, NULL, NULL),
+(36, 18, 'Terdapat Akses Kendaraan Umum Namun Tidak Setiap Saat', 4, 1, NULL, NULL, NULL, NULL),
+(37, 18, 'Tidak Terdapat Akses Kendaraan Umum Namun Lalu Lintas Lancar', 3, 1, NULL, NULL, NULL, NULL),
+(38, 18, 'Tidak Terdapat Akses Kendaraan Umum Dan Lalu Lintas Tidak Lancar', 1, 1, NULL, NULL, NULL, NULL),
+(39, 19, '< 2 km', 5, 1, NULL, NULL, NULL, NULL),
+(40, 19, '2.1 km – 4 km', 4, 1, NULL, NULL, NULL, NULL),
+(41, 19, '4.1 km – 6 km', 3, 1, NULL, NULL, NULL, NULL),
+(42, 19, '6.1 km – 8 km', 2, 1, NULL, NULL, NULL, NULL),
+(43, 19, '> 8 km', 1, 1, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -233,7 +297,7 @@ ALTER TABLE `alternative`
 -- AUTO_INCREMENT for table `alternative_value`
 --
 ALTER TABLE `alternative_value`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT for table `criteria`
@@ -245,7 +309,7 @@ ALTER TABLE `criteria`
 -- AUTO_INCREMENT for table `criterion_value`
 --
 ALTER TABLE `criterion_value`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- AUTO_INCREMENT for table `users`
