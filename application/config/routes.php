2737106a892ca26f34bac18b2d14856dd1bbc43f
dodinovembri <dodinovembri32gmail.com>
2021-02-23 13:07:11 +0700

    update

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,11 +49,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'authcontroller';
+$route['default_controller'] = 'AuthController';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
-$route['index'] = 'authcontroller/index';
+$route['index'] = 'AuthController/index';
 $route['login'] = 'authcontroller';
 $route['register'] = 'authcontroller/register';
 
