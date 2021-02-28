0e1b885bd7013a295d0efc7160d67831391fd1df
dodinovembri <dodinovembri32@gmail.com>
2021-02-28 18:31:33 +0700

    update

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -1,17 +1,14 @@
-// $(document).ready(function() {
-//     $('#ranking').DataTable( {    	        
-//         "order": [2, "desc"]
-//     } );
-// } );
-
 $(document).ready(function() {
-    var t = $('#ranking').DataTable( {
-        "order": [ 3, 'desc' ]
-    } );
+	$('#ranking').DataTable({
+		"order": [1, 'desc']
+	})
+    // var t = $('#ranking').DataTable( {
+    //     "order": [ 3, 'desc' ]
+    // } );
  
-    t.on( 'order.dt search.dt', function () {
-        t.column(0, {search:'applied', order:'applied'}).nodes().each( function (cell, i) {
-            cell.innerHTML = i+1;
-        } );
-    } ).draw();
+    // t.on( 'order.dt search.dt', function () {
+    //     t.column(0, {search:'applied', order:'applied'}).nodes().each( function (cell, i) {
+    //         cell.innerHTML = i+1;
+    //     } );
+    // } ).draw();
 } );
